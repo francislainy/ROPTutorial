5fedfd37223da5707562f9000973da309199d8ee
Francislainy <francislainy.campos@gmail.com>
2016-12-26 15:38:49 +0000

    Added Submit Button

--- app/src/main/java/com/example/android/roptutorial/SurveyActivity.java
+++ app/src/main/java/com/example/android/roptutorial/SurveyActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.roptutorial;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class SurveyActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class SurveyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_survey);
+
+        Button submitButton = (Button) findViewById(R.id.submit_survey_button);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+
     }
 }

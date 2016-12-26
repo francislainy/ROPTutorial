dd3bc1a8b727b935fc97eaf72ece58e0293fef32
Francislainy <francislainy.campos@gmail.com>
2016-12-26 16:24:56 +0000

    Added CheckBox to SurveyActivity

--- app/src/main/java/com/example/android/roptutorial/SurveyActivity.java
+++ app/src/main/java/com/example/android/roptutorial/SurveyActivity.java
@@ -3,6 +3,8 @@ package com.example.android.roptutorial;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
 import android.widget.RadioGroup;
 
 public class SurveyActivity extends AppCompatActivity {
@@ -13,11 +15,9 @@ public class SurveyActivity extends AppCompatActivity {
         setContentView(R.layout.activity_survey);
 
         RadioGroup rg = (RadioGroup) findViewById(R.id.premature_birth_button);
-
-        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
-        {
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                switch(checkedId){
+                switch (checkedId) {
                     case R.id.premature_birth_button_yes:
                         // do operations specific to this selection
                         Log.v("SurveyActivity", "yes");
@@ -29,6 +29,50 @@ public class SurveyActivity extends AppCompatActivity {
                 }
             }
         });
+
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.tutorial_button);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedID) {
+                switch (checkedID) {
+                    case R.id.tutorial_yes:
+                        Log.v("SurveyActivity", "yes tutorial");
+                        break;
+                    case R.id.tutorial_no:
+                        Log.v("SurveyActivity", "no tutorial");
+                        break;
+                }
+            }
+        });
+
+        final CheckBox expectingChildCheckBox = (CheckBox) findViewById(R.id.expecting_child_checkbox);
+        expectingChildCheckBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(expectingChildCheckBox.isChecked())
+                    Log.v("SurveyActivity", "expecting cb selected");
+            }
+        });
+
+        final CheckBox prematureNewbornCheckbox = (CheckBox) findViewById(R.id.premature_newborn_checkbox);
+        prematureNewbornCheckbox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(prematureNewbornCheckbox.isChecked())
+                    Log.v("SurveyActivity", "newborn cb selected");
+            }
+        });
+
+        final CheckBox knowSomeoneCheckBox = (CheckBox) findViewById(R.id.know_someone_checkbox);
+        knowSomeoneCheckBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(knowSomeoneCheckBox.isChecked())
+                    Log.v("SurveyActivity", "know someone cb selected");
+            }
+        });
+
+
 
     }
 
